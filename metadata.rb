2e1f4ee9d1ff97c85af0659e305b13d5661d6497
Jake Plimack <Jake.plimack@gmail.com>
2015-03-25 12:17:34 -0600

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,21 @@
+lib = File.expand_path('../../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'berkshelf/api/version'
+
+name             "berkshelf-api-server"
+maintainer       "Jamie Winsor"
+maintainer_email "jamie@vialstudios.com"
+license          "Apache 2.0"
+description      "Installs/Configures a berkshelf-api server"
+long_description "Installs/Configures a berkshelf-api server"
+version          Berkshelf::API::VERSION
+
+%w{ redhat centos ubuntu }.each do |os|
+  supports os
+end
+
+depends "runit"
+depends "build-essential", ">= 2.0.2"
+depends "nginx",           ">= 1.7.0"
+depends "libarchive",      "~> 0.4"
+depends "github",          "~> 0.3"
